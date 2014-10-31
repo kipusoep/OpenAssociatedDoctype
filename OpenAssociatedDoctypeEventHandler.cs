5187c07021346370940e997999ebd3542ac59ccf
kipusoep <mail@kipusoep.nl>
2014-10-31 16:48:35 +0100

    Initial commit

---
+++ OpenAssociatedDoctypeEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Trees;
+
+namespace InfoCaster.Umbraco.OpenAssociatedDoctype
+{
+    public class OpenAssociatedDoctypeEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            base.ApplicationStarted(umbracoApplication, applicationContext);
+
+            ContentTreeController.MenuRendering += ContentTreeController_MenuRendering;
+        }
+
+        void ContentTreeController_MenuRendering(TreeControllerBase sender, MenuRenderingEventArgs e)
+        {
+            MenuItem menuItem = new MenuItem("assDocType", "Open associated DocType")
+            {
+                Icon = "wrench"
+            };
+            menuItem.AdditionalData.Add("actionRoute", string.Format("/settings/framed/%2Fumbraco%2Fsettings%2FeditNodeTypeNew.aspx%3Fid%3D{0}", ApplicationContext.Current.Services.ContentService.GetById(int.Parse(e.NodeId)).ContentTypeId));
+            e.Menu.Items.Add(menuItem);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InfoCaster Umbraco OpenAssociatedDoctype")]
+[assembly: AssemblyDescription("InfoCaster Umbraco OpenAssociatedDoctype")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("InfoCaster")]
+[assembly: AssemblyProduct("OpenAssociatedDoctype")]
+[assembly: AssemblyCopyright("Copyright © InfoCaster")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("823e4b9f-c666-4760-9ca5-f1c8bb5ad811")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
